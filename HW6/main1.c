fd4c8587f0f6b7db7b15d481064f6cf7a6335bf8
Li220Fish <119789640+Li220Fish@users.noreply.github.com>
2022-12-21 21:58:48 +0800

    Create main1.c

---
+++ HW6/main1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int i = 10;
+    if (i % 15 == 0) 
+        printf("Love IU");
+    esle if (i % 5 == 0)
+        printf("IU");
+    else if (i % 3 == 0)
+        printf("Love");
+    esle 
+        printf("%d\n,i);
+               
+    return 0;
+}
+      
+      
+  
