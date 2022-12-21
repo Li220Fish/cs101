efe44d1e4c8ee70f481aca6728af092f86123745
Li220Fish <119789640+Li220Fish@users.noreply.github.com>
2022-12-21 22:12:50 +0800

    Create main3.c

---
+++ HW6/main3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int year = 2022;
+    if(year%400 == 0)
+      printf("yes");
+
+    if(year%4 == 0 & year%100 !=0)
+      printf("yes");
+    else
+      printf("No");
+}
