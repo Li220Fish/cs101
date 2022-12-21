1269f627ed6649724bf64156f5b13f55e98cf218
Li220Fish <119789640+Li220Fish@users.noreply.github.com>
2022-12-21 22:01:09 +0800

    Update main1.c

--- HW6/main1.c
+++ HW6/main1.c
@@ -3,12 +3,12 @@ int main() {
     int i = 10;
     if (i % 15 == 0) 
         printf("Love IU");
-    esle if (i % 5 == 0)
+    else if (i % 5 == 0)
         printf("IU");
     else if (i % 3 == 0)
         printf("Love");
-    esle 
-        printf("%d\n,i);
+    else 
+        printf("%d\n",i);
                
     return 0;
 }
